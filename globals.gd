3437eea072b2e17c0f3bd0937cbcdae30ac44f82
Stefano Corno <stlecorno@gmail.com>
2025-07-23 12:25:37 -0700

    added config file

--- globals.gd
+++ globals.gd
@@ -1,3 +1,14 @@
 extends Node
 
 var otherPlayerId = -1
+var ip : String = "127.0.0.1"
+var port : int = 4242
+
+func _ready():
+	load_config()
+
+func load_config() -> void:
+	var config = ConfigFile.new()
+	if config.load("res://config.cfg") == OK:
+		ip = config.get_value("network", "server_ip", ip)
+		port = config.get_value("network", "server_port", port)
