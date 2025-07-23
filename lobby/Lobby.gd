1d89eb093ad332674e1fd4ff22fc2e6c533c9630
Stefano Corno <stlecorno@gmail.com>
2025-07-23 16:52:06 -0700

    multiplayer paddle refactor

--- lobby/Lobby.gd
+++ lobby/Lobby.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	if OS.has_feature("server"):
-		pass # TODO: Switch to server implementation
+		print("Running in server mode") # TODO: Switch to server implementation
 
 func _on_LocalButton_pressed():
 	get_tree().change_scene("res://LocalWorld.tscn")
--- paddle/paddle_multiplayer.gd
+++ paddle/paddle_multiplayer.gd
@@ -1,11 +1,9 @@
-extends KinematicBody2D
+extends "res://paddle/paddle_base.gd"
 
-const SPEED = 250
+var control : bool = true
+var server_y_pos : int = 300
 
-var control = true
-var move_dir = 0
-
-func _physics_process(delta):
+func read_input() -> void:
 	if not control: return
 	
 	if Input.is_action_pressed("l_up"):
@@ -14,11 +12,14 @@ func _physics_process(delta):
 		move_dir = 1
 	else: move_dir = 0
 	
-	position.y = clamp(position.y, 0, 600)
-	var movement = Vector2(0, move_dir * SPEED * delta)
-	move_and_collide(movement)
-	if (globals.otherPlayerId > 1):
-		rpc_unreliable_id(globals.otherPlayerId, "do_move", position.y)
+	# TODO: Send input to server
+	#if (globals.otherPlayerId > 1):
+	#	rpc_unreliable_id(globals.otherPlayerId, "do_move", position.y)
+
+func update_position(delta: float) -> void:
+	if control:
+		var movement = Vector2(0, move_dir * SPEED * delta)
+		move_and_collide(movement)
 
 remote func do_move(y_pos):
 	position.y = y_pos
